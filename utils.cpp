26accd12ea8224219630dc670280880a5c438771
Antoine <60519774+aalp75@users.noreply.github.com>
2024-11-06 13:37:45 +0100

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -9,7 +9,7 @@ const int N = 1e3;
 int MAX_INT = 1e9;
 
 /**
- * modular expression
+ * modular operation
  */
 
 ll add(const ll& x, const ll& y) { // x + y
@@ -28,8 +28,59 @@ ll inverse(const ll& x) { // 1 / x
     return x <= 1 ? x : MOD - (ll)(MOD / x) * inverse(MOD % x) % MOD; 
 }
 
-ll div(const ll& x, const ll& y) { // x / y
+ll division(const ll& x, const ll& y) { // x / y
     return mul(x, inverse(y));
+}
+
+ll fast_exponentiation(const ll& x, const ll& y) { // x ^ y
+    if (y == 0) return 1;
+    if (y == 1) return x;
+    if (y % 2 == 0) {
+        return fast_exponentiation(mul(x, x), y / 2);
+    }
+    return mul(x, fast_exponentiation(x, y - 1));
+}
+
+/**
+ * modular arithmetic
+ */
+
+ll gcd(const ll& x, const ll& y) {
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+ll lcm(const ll& x, const ll& y) {
+    return x / gcd(x, y) * y;
+}
+
+/**
+ * polynomial hashing
+ * 
+ * MOD = 1e9 + 9 is recommended
+ * base = 2 for binary string
+ * base = 31 for low characters string
+ */
+
+ll compute_hash(vector<int>& s, const ll& base) {
+    ll hash_value = 0;
+    ll base_power = 1;
+    for (auto value : s) {
+        hash_value = add(hash_value, mul(value, base_power));
+        base_power = mul(base_power, base);
+    }
+    return hash_value;
+}
+
+ll compute_hash(const string& s, const ll& base) {
+    ll hash_value = 0;
+    ll base_power = 1;
+    for (auto c : s) {
+        ll value = c -'a' + 1;
+        hash_value = add(hash_value, mul(value, base_power));
+        base_power = mul(base_power, base);
+    }
+    return hash_value;
 }
 
 /**
@@ -74,3 +125,10 @@ void update(int k, int x) {
     }
 }
 
+/**
+ * 
+ */
+
+int main() {
+    return 0;
+}
