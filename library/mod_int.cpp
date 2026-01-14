169e1b6d31d98f68cf4b4c96c21119c4108879d4
aalp75 <antoine.lepeltier75@hotmail.fr>
2026-01-14 11:32:33 +0100

    add mod int

--- library/mod_int.cpp
+++ library/mod_int.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+const long long MOD = 7;
+
 using namespace std;
 
 /**
@@ -7,10 +9,13 @@ using namespace std;
  * 
  * MOD needs to be a prime number so every number has an inverse modulo MOD
  * 
- * ModInt<998244353> x = 3;
+ * Usage:
  * 
  * using mint = ModInt<998244353>
  * mint x = 3;
+ * 
+ * ModInt<998244353> x = 3;
+ * 
  */
 
 template <long long MOD>
@@ -20,18 +25,42 @@ struct ModInt {
 
     ModInt() : val(0) {}
 
-    ModInt(long long val_) : val(val_ % MOD) {}
+    ModInt(long long v) : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
 
-    // unary
+    // unary operators
     ModInt operator+() const {
         return *this;
     }
     
     ModInt operator-() const {
-        return ModInt(MOD - val * (val != 0));
+        return ModInt((val == 0) ? 0 : MOD - val);
     }
 
-    // in-place ops
+    // in-place operators
+    ModInt& operator+=(const ModInt& other) {
+        val += other.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+
+    ModInt& operator-=(const ModInt& other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    ModInt& operator*=(const ModInt& other) {
+        val = (long long)(__int128(val) * other.val % MOD);
+        return *this;
+    }
+
+    ModInt& operator/=(const ModInt& other) {
+        return *this *= other.inverse();
+    }
+
+    // binary operators (friends)
     friend ModInt operator+(ModInt a, const ModInt& b) {
         return a += b;
     }
@@ -39,6 +68,7 @@ struct ModInt {
     friend ModInt operator-(ModInt a, const ModInt& b) {
         return a -= b; 
     }
+
     friend ModInt operator*(ModInt a, const ModInt& b) { 
         return a *= b; 
     }
@@ -46,15 +76,116 @@ struct ModInt {
     friend ModInt operator/(ModInt a, const ModInt& b) { 
         return a /= b;
     }
-    
 
+    // allow operations with long long on the right
+    friend ModInt operator+(ModInt a, long long b) { return a += ModInt(b); }
+    friend ModInt operator-(ModInt a, long long b) { return a -= ModInt(b); }
+    friend ModInt operator*(ModInt a, long long b) { return a *= ModInt(b); }
+    friend ModInt operator/(ModInt a, long long b) { return a /= ModInt(b); }
 
+    // allow operations with long long on the left
+    friend ModInt operator+(long long a, ModInt b) { return ModInt(a) += b; }
+    friend ModInt operator-(long long a, ModInt b) { return ModInt(a) -= b; }
+    friend ModInt operator*(long long a, ModInt b) { return ModInt(a) *= b; }
+    friend ModInt operator/(long long a, ModInt b) { return ModInt(a) /= b; }
+
+    // comparison operators
+    friend bool operator==(const ModInt& a, const ModInt& b) {
+        return a.val == b.val;
+    }
+
+    friend bool operator!=(const ModInt& a, const ModInt& b) {
+        return a.val != b.val;
+    }
+
+    friend bool operator<(const ModInt& a, const ModInt& b) {
+        return a.val < b.val;
+    }
+
+    friend bool operator>(const ModInt& a, const ModInt& b) {
+        return a.val > b.val;
+    }
+
+    friend bool operator<=(const ModInt& a, const ModInt& b) {
+        return a.val <= b.val;
+    }
+
+    friend bool operator>=(const ModInt& a, const ModInt& b) {
+        return a.val >= b.val;
+    }
+
+    // increment / decrement operators
+    ModInt& operator++() {
+        return *this += 1;
+    }
+
+    ModInt& operator--() {
+        return *this -= 1;
+    }
+
+    ModInt operator++(int) {
+        ModInt tmp = *this;
+        ++*this;
+        return tmp;
+    }
+
+    ModInt operator--(int) {
+        ModInt tmp = *this;
+        --*this;
+        return tmp;
+    }
+
+    // inverse
+    // x ^ -1 based on Euclidean division
+    ModInt inverse() const { 
+        long long x = val;
+        assert(x != 0);
+        if (x == 1) return 1;
+        return ModInt(MOD - (MOD / x) * ModInt(MOD % x).inverse().val % MOD);
+    }
+
+    // fast exponentiation
+    // this ^ exp
+    ModInt power(long long exp) const { 
+        ModInt base = *this;
+        ModInt res = 1;
+        while (exp > 0) {
+            if (exp & 1) 
+                res *= base;
+            base *= base;
+            exp >>= 1;
+        }
+        return res;
+    }
+
+    // I/O
+    friend ostream& operator<<(ostream& os, const ModInt& x) {
+        return os << x.val;
+    }
+    friend istream& operator>>(istream& is, ModInt& x) {
+        long long v;
+        is >> v;
+        x = ModInt(v);
+        return is;
+    }
+
+    // cast
+    operator long long() const {
+        return val;
+    }
 };
 
+using mint = ModInt<MOD>;
 
 int main() {
-    using mint = ModInt<998244353>;
-    mint x = 3;
 
-    cout << x.val << '\n';
+    mint x = 3;
+    mint y = 5;
+
+    cout << "x = " << x << ", y = " << y << '\n';
+
+    cout << "x + y = " << x + y << '\n';
+    cout << "x - y = " << x - y << '\n';
+    cout << "x * y = " << x * y << '\n';
+    cout << "x / y = " << x / y << '\n';
 }
