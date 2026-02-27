8d2a7b5d94c3e0f25e67ea85cc68e145988bcfd0
aalp75 <antoine.lepeltier75@hotmail.fr>
2026-02-27 21:35:02 +0100

    update primes

--- library/primes.cpp
+++ library/primes.cpp
@@ -24,20 +24,31 @@ void prime_sieve() {
     }
 }
 
-vector<int> prime_factors(N, 1);
+vector<int> spf(N, 1); // smallest prime factor
 
 void prime_factor_sieve() {
     vector<bool> is_prime(N, true);
     for (int i = 2; i < N; i++) {
         if (!is_prime[i]) continue;
         primes.push_back(i);
-        prime_factors[i] = i;
+        spf[i] = i;
         for (int j = 2 * i; j < N; j += i) {
             is_prime[j] = false;
-            if (prime_factors[j] == 1) prime_factors[j] = i;
+            if (spf[j] == 1) prime_fspfactors[j] = i;
         }
     }
 }
+
+/**
+ * pre compute all divisors of all numbers
+ * 
+ * Complexity is O(n log(n)) due to the harmonic series 
+ * 
+ * it's ok for N ~ 1e5 but a bit slow for N ~ 1e6 due to the lot of
+ * push_back reallocation and bad cache locacity.
+ * 
+ * If constraints is up to 1e6, it's better to use another approach
+ */
 
 vector<vector<int>> divisors(N);
 
